100fe870d35d4bd99cbdef8821c8caf25c1ec795
Diogo Biazus <diogob@gmail.com>
2014-07-03 15:35:28 -0400

    created method to get the user (recipient) association name, now we use it in notification creation

--- app/models/user_notifier/base.rb
+++ app/models/user_notifier/base.rb
@@ -1,6 +1,5 @@
 class UserNotifier::Base < ActiveRecord::Base
   self.abstract_class = true
-  belongs_to UserNotifier.user_class_name
 
   def self.notify_once(template_name, user, filter, params = {})
     notify(template_name, user, params) if is_unique?(template_name, filter)
@@ -11,7 +10,8 @@ class UserNotifier::Base < ActiveRecord::Base
       template_name: template_name,
       locale: I18n.locale,
       from_email: UserNotifier.from_email,
-      from_name: UserNotifier.from_name
+      from_name: UserNotifier.from_name,
+      self.user_association_name => user
     }.merge(params)).tap{|n| n.deliver }
   end
 
@@ -21,10 +21,15 @@ class UserNotifier::Base < ActiveRecord::Base
 
   def deliver!
     UserNotifier::EmailWorker.perform_async(self.class.name.to_s, self.id)
-    self.update_attributes sent_at: Time.now()
   end
 
   private
+  def self.user_association_name
+    UserNotifier.user_class_name.downcase.to_sym
+  end
+
+  belongs_to user_association_name
+
   def self.is_unique?(template_name, filter)
     filter.nil? || self.where(filter.merge(template_name: template_name)).empty?
   end
