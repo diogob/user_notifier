7c98ae8be3d97bcbd31cfb430946a49852c0eb17
Antônio Roberto <forevertonny@gmail.com>
2014-11-18 09:51:51 -0200

    small refactor and added method to push a header for xstmp-api

--- app/mailers/user_notifier/mailer.rb
+++ app/mailers/user_notifier/mailer.rb
@@ -3,21 +3,47 @@ class UserNotifier::Mailer < ActionMailer::Base
 
   def notify(notification)
     @notification = notification
+
     old_locale = I18n.locale
     I18n.locale = @notification.locale
-    subject = render_to_string(template: "user_notifier/mailer/#{@notification.template_name}_subject")
-    m = mail({
+
+    configure_xsmtp_headers if UserNotifier.use_xsmtp_api
+
+    message = mail(mail_attributes)
+
+    I18n.locale = old_locale
+
+    message
+  end
+
+  private
+
+  def subject
+    render_to_string(template: "user_notifier/mailer/#{@notification.template_name}_subject")
+  end
+
+  def mail_attributes
+    {
       from: address_format(UserNotifier.system_email, @notification.from_name),
       reply_to: address_format(@notification.from_email, @notification.from_name),
       to: @notification.user.email,
       subject: subject,
       template_name: @notification.template_name
-    })
-    I18n.locale = old_locale
-    m
+    }
   end
 
-  private
+
+  def configure_xsmtp_headers
+    headers['X-SMTPAPI'] = {
+      unique_args: {
+        notification_user: @notification.user_id,
+        notification_type: @notification.class.to_s,
+        notification_id: @notification.id,
+        template_name: @notification.template_name
+      }
+    }.to_json
+  end
+
   def address_format email, name
     address = Mail::Address.new email
     address.display_name = name
