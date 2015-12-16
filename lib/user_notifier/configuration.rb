d4e73d1b8dcd898b1a23ca0f4888b3908371e450
Diogo Biazus <diogob@gmail.com>
2015-12-16 17:07:19 -0500

    Includes configuration parameter to decide if should install after_commit hook in Notification models

--- lib/user_notifier/configuration.rb
+++ lib/user_notifier/configuration.rb
@@ -1,13 +1,14 @@
 module UserNotifier
   module Configuration
-    VALID_CONFIG_KEYS    = [:system_email, :email_layout, :user_class_name, :from_email, :from_name, :use_xsmtp_api].freeze
+    VALID_CONFIG_KEYS    = [:system_email, :email_layout, :user_class_name, :from_email, :from_name, :use_xsmtp_api, :auto_deliver].freeze
 
     DEFAULT_SYSTEM_EMAIL      = nil
     DEFAULT_EMAIL_LAYOUT      = 'email'
     DEFAULT_USER_CLASS_NAME   = 'User'
     DEFAULT_FROM_EMAIL        = 'no-reply@yourdomain'
     DEFAULT_FROM_NAME         = 'please configure a default from name'
-    DEFAULT_USE_XSMTP_API      = false
+    DEFAULT_USE_XSMTP_API     = false
+    DEFAULT_AUTO_DELIVER      = true
 
     # Build accessor methods for every config options so we can do this, for example:
     #   Awesome.format = <img src="http://s2.wp.com/wp-includes/images/smilies/icon_mad.gif?m=1129645325g" alt=":x" class="wp-smiley"> ml
@@ -33,6 +34,7 @@ module UserNotifier
       self.from_email         = DEFAULT_FROM_EMAIL
       self.from_name          = DEFAULT_FROM_NAME
       self.use_xsmtp_api      = DEFAULT_USE_XSMTP_API
+      self.auto_deliver       = DEFAULT_AUTO_DELIVER
     end
 
   end # Configuration
